2115cbbb79bf7191fa65a09aeae04a1a1d71d73e
user <user@marvin>
2021-03-21 19:07:11 -0600

    stair icons

--- tools/iconcube.py
+++ tools/iconcube.py
@@ -2,7 +2,7 @@
 
 """
     mc4, a web voxel building game
-    Copyright (C) 2019 kholland4
+    Copyright (C) 2019-2021 kholland4
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -21,6 +21,7 @@
 import sys, pygame, time, math
 
 slab = False
+stair = False
 
 face1 = None
 face2 = None
@@ -78,22 +79,61 @@ h = int(u * 1.5)
 
 out_xoff = u - w
 
+if stair:
+    newFace2 = pygame.Surface((16, 16), pygame.SRCALPHA)
+    newFace2.blit(face2, (0, 0))
+    face2 = newFace2
+    pygame.draw.rect(face2, (0, 0, 0, 0), pygame.Rect(8, 0, 8, 8))
 f2 = skew(face2, pygame.Surface((w, h), pygame.SRCALPHA), int(u / 2), u)
 f2.fill((200, 200, 200, 255), None, pygame.BLEND_RGBA_MULT)
 out_surf.blit(f2, (out_xoff, output_res[1] - h))
 
-f3 = skew(face3, pygame.Surface((w, h), pygame.SRCALPHA), -int(u / 2), u)
-f3.fill((160, 160, 160, 255), None, pygame.BLEND_RGBA_MULT)
-out_surf.blit(f3, (out_xoff + w, output_res[1] - h))
+if stair:
+    newFace3_top = pygame.Surface((16, 16), pygame.SRCALPHA)
+    newFace3_top.blit(face3, (0, 0))
+    pygame.draw.rect(newFace3_top, (0, 0, 0, 0), pygame.Rect(0, 8, 16, 8))
+    
+    f3_top = skew(newFace3_top, pygame.Surface((w, h), pygame.SRCALPHA), -int(u / 2), u)
+    f3_top.fill((160, 160, 160, 255), None, pygame.BLEND_RGBA_MULT)
+    out_surf.blit(f3_top, (out_xoff + int(w / 2), output_res[1] - h - int(u / 4)))
+    
+    newFace3_bottom = pygame.Surface((16, 16), pygame.SRCALPHA)
+    newFace3_bottom.blit(face3, (0, 0))
+    pygame.draw.rect(newFace3_bottom, (0, 0, 0, 0), pygame.Rect(0, 0, 16, 8))
+    
+    f3_bottom = skew(newFace3_bottom, pygame.Surface((w, h), pygame.SRCALPHA), -int(u / 2), u)
+    f3_bottom.fill((160, 160, 160, 255), None, pygame.BLEND_RGBA_MULT)
+    out_surf.blit(f3_bottom, (out_xoff + w, output_res[1] - h))
+else:
+    f3 = skew(face3, pygame.Surface((w, h), pygame.SRCALPHA), -int(u / 2), u)
+    f3.fill((160, 160, 160, 255), None, pygame.BLEND_RGBA_MULT)
+    out_surf.blit(f3, (out_xoff + w, output_res[1] - h))
 
-f1 = pygame.Surface((300, 300), pygame.SRCALPHA)
-f1.blit(pygame.transform.scale(face1, (300, 300)), (0, 0))
-f1 = pygame.transform.rotate(f1, 45)
-f1 = pygame.transform.scale(f1, (int(u * math.sqrt(3)), u))
-f1_yoff = 0
-if slab:
-    f1_yoff = u / 2
-out_surf.blit(f1, (out_xoff, f1_yoff))
+if stair:
+    f1a = pygame.Surface((300, 300), pygame.SRCALPHA)
+    f1a.blit(pygame.transform.scale(face1, (300, 300)), (0, 0))
+    pygame.draw.rect(f1a, (0, 0, 0, 0), pygame.Rect(0, 150, 300, 150))
+    f1a = pygame.transform.rotate(f1a, 45)
+    f1a = pygame.transform.scale(f1a, (int(u * math.sqrt(3)), u))
+    f1a_yoff = 0
+    out_surf.blit(f1a, (out_xoff, f1a_yoff))
+    
+    f1b = pygame.Surface((300, 300), pygame.SRCALPHA)
+    f1b.blit(pygame.transform.scale(face1, (300, 300)), (0, 0))
+    pygame.draw.rect(f1b, (0, 0, 0, 0), pygame.Rect(0, 0, 300, 150))
+    f1b = pygame.transform.rotate(f1b, 45)
+    f1b = pygame.transform.scale(f1b, (int(u * math.sqrt(3)), u))
+    f1b_yoff = u / 2
+    out_surf.blit(f1b, (out_xoff, f1b_yoff))
+else:
+    f1 = pygame.Surface((300, 300), pygame.SRCALPHA)
+    f1.blit(pygame.transform.scale(face1, (300, 300)), (0, 0))
+    f1 = pygame.transform.rotate(f1, 45)
+    f1 = pygame.transform.scale(f1, (int(u * math.sqrt(3)), u))
+    f1_yoff = 0
+    if slab:
+        f1_yoff = u / 2
+    out_surf.blit(f1, (out_xoff, f1_yoff))
 
 screen.fill((0, 0, 0))
 screen.blit(out_surf, (0, 0))
